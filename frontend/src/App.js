a9d54d91fc74191abb12e5243c98ffa1b531f46e
AlexTech314 <alex.s@wustl.edu>
2020-11-17 17:45:03 -0600

    15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               amazona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
-          <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
@@ -29,5 +38,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Product(props) {
   const { product } = props;
   return (
     <div key={product._id} className="card">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <img className="medium" src={product.image} alt={product.name} />
-      </a>
+      </Link>
       <div className="card-body">
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <h2>{product.name}</h2>
-        </a>
+        </Link>
         <div className="price">${product.price}</div>
       </div>
     </div>
