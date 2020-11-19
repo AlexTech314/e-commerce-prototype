aafa5c0a56fc71c9b44b8feac3cb54839a6e2fae
AlexTech314 <alex.s@wustl.edu>
2020-11-18 20:07:29 -0600

    almost

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,10 @@
 import {
     CART_ADD_ITEM,
+    CART_EMPTY,
     CART_REMOVE_ITEM,
     CART_SAVE_PAYMENT_METHOD,
     CART_SAVE_SHIPPING_ADDRESS,
   } from '../constants/cartConstants';
-  
   export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
       case CART_ADD_ITEM:
@@ -29,6 +29,8 @@ import {
         return { ...state, shippingAddress: action.payload };
       case CART_SAVE_PAYMENT_METHOD:
         return { ...state, paymentMethod: action.payload };
+      case CART_EMPTY:
+        return { ...state, cartItems: [] };
       default:
         return state;
     }
