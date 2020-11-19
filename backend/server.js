4b22b36ceb2df4c739d2e44e50f63723b287c157
AlexTech314 <alex.s@wustl.edu>
2020-11-18 20:01:38 -0600

    almost judgement day

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
-import dotenv from 'dotenv';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,14 +18,13 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
