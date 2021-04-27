28bd3d50932ce99d2b08cf5196652cd30f93e30a
AlexTech314 <alex.s@wustl.edu>
2021-04-27 18:39:12 -0500

    commiting to main, need test mongo server

--- backend/server.js
+++ backend/server.js
@@ -38,12 +38,12 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-// app.use(express.static(path.join(__dirname, "/frontend/build")));
-// app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/frontend/build/index.html")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/frontend/build/index.html")));
 
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
